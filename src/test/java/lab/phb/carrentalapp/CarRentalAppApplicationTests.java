d195b0e89f09731aba26c9f7661edfddb0e7fd6a
tamami <tamami.oka@gmail.com>
2017-12-29 13:32:55 +0700

    init commit

---
+++ src/test/java/lab/phb/carrentalapp/CarRentalAppApplicationTests.java
@@ -0,0 +1,16 @@
+package lab.phb.carrentalapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarRentalAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
